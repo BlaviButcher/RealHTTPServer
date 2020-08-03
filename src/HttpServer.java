b0a8d42bae2efd1db42aa6c5eb18e63641d1b4f6
Douglas, Riley James <rileydouglas@outlook.com>
2020-08-03 17:12:44 +1200

    Added functionality to accept connection and spawn thread. Displaying success and ip of client.

--- src/HttpServer.java
+++ src/HttpServer.java
@@ -1,13 +1,30 @@
 import java.net.*;
 import java.io.*;
 import java.util.*;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class HttpServer
 {
+    private static final ExecutorService exec =  Executors.newFixedThreadPool(10);
     public static void main(String args[])
     {
         try{
+            // Create serverSocket
             ServerSocket server = new ServerSocket(8080);
+            while(true){
+                // Wait for something to connect
+                Socket client = server.accept();
+                // Create new ServerSession of connected client
+                HttpServerSession task = new HttpServerSession(client);
+
+                // Get ip address of client to display
+                InetAddress ip = client.getInetAddress();
+                System.out.println("Connection created with "+ ip);
+
+                // run thread
+                exec.execute(task);
+            }
         }
         catch (IOException e)
         {
--- src/HttpServerSession.java
+++ src/HttpServerSession.java
@@ -1,3 +1,18 @@
-public class HttpServerSession
+import java.net.*;
+import java.io.*;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ExecutorService;
+public class HttpServerSession implements Runnable
 {
+
+    Socket client;
+    @Override
+    public void run()
+    {
+        System.out.println("Connection accepted");
+    }
+    public HttpServerSession(Socket s)
+    {
+        client = s;
+    }
 }
